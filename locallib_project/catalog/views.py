778cbfdeb2d8e43a47c3b9179c7494032a59bf40
Tanner Yould <tanner.yould@gmail.com>
2020-05-28 13:47:51 -0700

    p5 no challenge yet

--- locallib_project/catalog/views.py
+++ locallib_project/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -30,3 +31,10 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
